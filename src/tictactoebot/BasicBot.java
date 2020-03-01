8d43e35f810a366848277803f4f4124b8ec32818
Edward Potapov <epotapov@comcast.net>
2020-02-29 22:39:17 -0700

    h

--- src/tictactoebot/BasicBot.java
+++ src/tictactoebot/BasicBot.java
@@ -13,18 +13,22 @@ import javafx.geometry.Point2D;
  * @author Edward Potapov
  */
 public class BasicBot {
-    class sections {
+    static class sections {
         int xwellness;
         int owellness;
-        sections (ArrayList<TheBoard.turnon> one, ArrayList<Point2D> two) {
+        sections (TheBoard.turnon[] one, Point2D[] two) {
             int xnumb = 0;
             int onumb = 0;
-            
-            for(int i = 0; i < one.size(); i++){
-                if(one.get(i).xturn)
+            Point2D location;
+            ArrayList<Point2D> locations = new ArrayList<Point2D>();
+            for(int i = 0; i < one.length; i++){
+                if(one[i].xturn)
                     xnumb++;
-                else if(one.get(i).oturn)
+                else if(one[i].oturn)
                     onumb++;
+                else {
+                    locations.add(two[i]);
+                }
             }
             if(xnumb > 0 && onumb > 0) {
                 xwellness = 0;
@@ -33,7 +37,16 @@ public class BasicBot {
                 xwellness = xnumb;
                 owellness = onumb;
             }
-            
+            if(xwellness > 0) {
+                Random r = new Random();
+                location = locations.get(r.nextInt(locations.size()));
+            }
+            if(owellness > 0) {
+                Random r = new Random();
+                location = locations.get(r.nextInt(locations.size()));
+            } else if (xwellness + owellness == 3) {
+                location = new Point2D(3,3);
+            }
         }
     }
     public static Point2D choosing () {
@@ -41,7 +54,17 @@ public class BasicBot {
         TheBoard.turnon[][] temp = TheBoard.board;
         ArrayList<Point2D> num = new ArrayList<Point2D>();
         ArrayList<sections> choose = new ArrayList<sections>();
+        choose.add(new sections(new TheBoard.turnon[]{temp[0][0],temp[0][1],temp[0][2]}, new Point2D[]{new Point2D(0,0), new Point2D(0,1), new Point2D(0,2)}));
+        choose.add(new sections(new TheBoard.turnon[]{temp[1][0],temp[1][1],temp[1][2]}, new Point2D[]{new Point2D(1,0), new Point2D(1,1), new Point2D(1,2)}));
+        choose.add(new sections(new TheBoard.turnon[]{temp[2][0],temp[2][1],temp[2][2]}, new Point2D[]{new Point2D(2,0), new Point2D(2,1), new Point2D(2,2)}));
+        choose.add(new sections(new TheBoard.turnon[]{temp[0][0],temp[1][0],temp[1][0]}, new Point2D[]{new Point2D(0,0), new Point2D(1,0), new Point2D(2,0)}));
+        choose.add(new sections(new TheBoard.turnon[]{temp[0][1],temp[1][1],temp[2][1]}, new Point2D[]{new Point2D(0,1), new Point2D(1,1), new Point2D(2,1)}));
+        choose.add(new sections(new TheBoard.turnon[]{temp[0][2],temp[1][2],temp[2][2]}, new Point2D[]{new Point2D(0,2), new Point2D(1,2), new Point2D(2,2)}));
+        choose.add(new sections(new TheBoard.turnon[]{temp[0][0],temp[1][1],temp[2][2]}, new Point2D[]{new Point2D(0,0), new Point2D(1,1), new Point2D(2,2)}));
+        choose.add(new sections(new TheBoard.turnon[]{temp[0][2],temp[1][1],temp[2][0]}, new Point2D[]{new Point2D(0,2), new Point2D(1,1), new Point2D(2,0)}));
         
+        for(sections s :choose) {
+        }
         for(int row = 0; row  < 3; row++) {
             for(int col = 0; col < 3; col++) {
                 if(!temp[row][col].oturn && !temp[row][col].xturn) {
